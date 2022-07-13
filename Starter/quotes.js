f9206880ea4073c47411132005e863c25582e07b
Mohamed <57545603+Mohamed475@users.noreply.github.com>
2022-07-13 23:23:20 +0200

    Update quotes.js

--- Starter/quotes.js
+++ Starter/quotes.js
@@ -1,9 +1,5 @@
 const localQuotes = [
   {
-    text: 'Genius is one percent inspiration and ninety-nine percent perspiration.',
-    author: 'Thomas Edison',
-  },
-  {
     text: 'You can observe a lot just by watching.',
     author: 'Yogi Berra',
   },
